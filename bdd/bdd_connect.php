a7d62941f6b2bdf7b81fe4002b8e2681a4b95893
mithridatem <mathieumithridate@adrar-formation.com>
2026-09-01 16:41:14 +0200

    feat: add bdd_connect.php file

--- bdd/bdd_connect.php
+++ bdd/bdd_connect.php
@@ -1,3 +1,13 @@
 <?php
 
 include '../env.php';
+
+function connect_bdd(): PDO
+{
+    return new PDO(
+        'mysql:host=' . BDD_HOST . ';dbname=' . BDD_NAME,
+        BDD_USERNAME,
+        BDD_PASSWORD,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+}
