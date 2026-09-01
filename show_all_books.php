5a98b4a94b42156a09c78bc1f58464bf2d372e58
mithridatem <mathieumithridate@adrar-formation.com>
2026-09-01 12:22:50 +0200

    feat: project structure and sql database script

---
+++ show_all_books.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h2>titre du livre</h2>
+        <div>sommaire du livre</div>
+        <h3>Nom auteur</h3>
+        <h3>date de publication</h3>
+        <h3>Editeur du livre</h3>
+        <h3>Catégorie du livre</h3>
+    </article>
+</body>
+</html>
