35761c0b0087f4db916532e6b7ce28b1e975cf94
mithridatem <mathieumithridate@adrar-formation.com>
2026-09-01 16:40:48 +0200

    feat: add navbar file

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Ajout éditeur</a></li>
+        <li><a href="">Ajout livre</a></li>
+        <li><a href="">Liste des livres</a></li>
+    </ul>
+</nav>
