b62a89597f3e206defb78e4b14d1c46c5dad14a4
mithridatem <mathieumithridate@adrar-formation.com>
2026-09-01 16:40:25 +0200

    feat: add model category

---
+++ model/category.php
@@ -0,0 +1,16 @@
+<?php
+include '../bdd/bdd_connect.php';
+
+//méthode pour récupérer la liste des catégories
+function get_all_categories(): array
+{
+    try {
+        $sql = "SELECT c.id, c.category_name FROM category AS c";
+        $req = connect_bdd()->prepare($sql);
+        $req->execute();
+        $categories = $req->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $categories ?? [];
+}
