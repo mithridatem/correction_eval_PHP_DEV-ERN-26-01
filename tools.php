b957851f6d0d4eb939b92bdb6cddb0acf1a679bb
mithridatem <mathieumithridate@adrar-formation.com>
2026-09-02 10:02:55 +0200

    feat: add sanitize_array method

--- tools.php
+++ tools.php
@@ -1,6 +1,13 @@
 <?php
 
+//Méthode pour sanitizer une chaîne de caractères
 function sanitize(string $str): string 
 {
-    return htmlspecialchars(ENT_NOQUOTES,strip_tags(trim($str)));
+    return htmlspecialchars(strip_tags(trim($str)), ENT_NOQUOTES);
+}
+
+//méthode pour sanitizer un tableau de chaînes de caractères
+function sanitize_array(array $arr): array 
+{
+    return array_map('sanitize', $arr);
 }
